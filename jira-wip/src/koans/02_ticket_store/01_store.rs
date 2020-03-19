430e5ca87c15ff2c54e12df59f291dd46307d163
Luca Palmieri <LukeMathWalker@users.noreply.github.com>
2020-03-19 08:48:18 +0000

    Second koans edits (#13)
    * Add ownership koan
    
    * Fix outdated comment.
    
    * Add exercise on derive
    
    * Add recap
    
    * Add persistence
    
    * Remove unused variable
    
    * Remove stand-alone mutability

--- jira-wip/src/koans/02_ticket_store/01_store.rs
+++ jira-wip/src/koans/02_ticket_store/01_store.rs
@@ -1,60 +1,75 @@
 mod store {
-    /// So far on this journey of building our own JIRA clone we have managed to complete quite a lot.
-    /// From building a Ticket to updating the visibility of our modules.
-    ///
-    /// Now we shift focus. Our tickets are doing well, but they need a home.
-    /// A place where we can store our tickets, search for them, and retrieve them.
+    /// It's time to shift focus: our tickets are doing well, but they need a home.
+    /// A place where we can store them, search for them, and retrieve them.
     ///
     /// We can use many different data structures to store and manage our tickets.
-    /// So it's wise to compare the pros and cons of data structures and select the best
-    /// ones that fit the use case.
-    ///
-    /// For us, we will have a ticket and some way to identify it, say with a unique identifier.
-    /// Therefore it makes sense for us to use a HashMap to store our tickets.
+    /// JIRA users rely heavily on ticket identifiers, e.g. RUST-2018 or COVID-19.
+    /// It's a unique label that identifies univocally a single ticket, generally `<board name>-<ticket number>`.
+    /// We don't have the concept of a board yet, so we'll roll with a simple numerical id.
+    /// 
+    /// What is the simplest data structure that allows us to fetch a ticket given its id?
+    /// It makes sense for us to use a HashMap, also known as a dictionary in other languages.
     /// You can read more about the HashMap in rust here: https://doc.rust-lang.org/std/collections/struct.HashMap.html
-    /// They work in a similar manner to HashMaps in other languages.
     use std::collections::HashMap;
+    /// Let's import what we worked on in the previous set of exercises.
+    use super::recap::Ticket;
 
     /*
     pub struct TicketStore {
         /// The collection of stored tickets.
-        ToDo
-        data: ,
+        data: __,
     }
     */
 
-    /// First we will create a TicketStore for tickets.
-    /// We can store an instance of the HashMap in a property of the TicketStore struct
-    /// The HashMap takes two generic parameters, one for the key, and one for the value <K, V>
+    /// First we will create a TicketStore struct, with a `data` field of type HashMap.
     ///
-    /// Let's set the value to the type of our Ticket, and feel free to choose whatever type you
-    /// would like for the key for now as we will update the key later.
+    /// HashMap is a *generic* struct: we need to specify two types, one for the key, and one for the stored value - HashMap<K, V>.
+    ///
+    /// Let's set the value to the type of our Ticket, and we will use an unsigned integer for our identifier.
     struct TicketStore {
         /// The collection of stored tickets.
         data: HashMap<u32, Ticket>,
     }
 
-    /// Now we can call a function to create a TicketStore
-    /// with a HashMap under the data property
-    fn create_ticket_store() -> TicketStore
-    {
-        TicketStore{
-            // Note that the compiler can infer the
-            // types for our HashMaps' key value pairs.
-            data: HashMap::new()
+    impl TicketStore {
+        /// Methods do not have to take self as a parameter.
+        /// This is the equivalent of a class/static method in other programming languages.
+        /// It can be invoked using `TicketStore::new()`.
+        pub fn new() -> TicketStore
+        {
+            TicketStore {
+                // Note that the compiler can infer the
+                // types for our HashMaps' key value pairs.
+                data: HashMap::new()
+            }
         }
-    }
 
-    struct Ticket {
-        title: String,
-        description: String
-    }
+        /// We take `&mut self` because we will have to mutate our HashMap to insert a new key/value pair.
+        pub fn save(&mut self, ticket: Ticket, id: u32)
+        {
+            self.data.insert(id, ticket);
+        }
 
+        pub fn get(&self, id: &u32) -> &Ticket {
+            self.data.get(id).expect("Failed to retrieve ticket")
+        }
+
+        /*
+        pub fn save(&mut self, ticket: Ticket, id: u32) -> TicketStore
+        {
+            todo!()
+        }
+
+        pub fn get(&self, id: &u32) -> &Ticket {
+            todo!()
+        }
+        */
+    }
 
     #[cfg(test)]
     mod tests {
-
         use super::*;
+        use super::super::recap::{create_ticket, Status};
 
         /// Now let's put our TicketStore to use
         ///
@@ -64,49 +79,36 @@ mod store {
         #[test]
         fn a_ticket_with_a_home()
         {
-            let ticket = Ticket {
-                title: "A ticket title".into(),
-                description: "An enlightened description".into()
-            };
+            let ticket = create_ticket("A ticket title".into(), "An enlightened description".into(), Status::ToDo);
 
-            // Pay special attention to the 'mut' keyword here.
-            // We have not encountered this keyword before, but don't worry
-            // we will get to it in the next section.
-            //
+            // Pay special attention to the 'mut' keyword here: variables are immutable by default in Rust.
+            // The `mut` keyword is used to signal that you must pay special attention to the variable as it's likely to change later on
+            // in the function body.
+            let mut store = TicketStore::new();
             // For now just note that if we wish to store anything in our HashMap
-            // we require the mut keyword when creating our TicketStore
-            let mut store = create_ticket_store();
             let ticket_id = 1;
 
-            store.data.insert(ticket_id, ticket);
+            // Here we need to create a clone of our `ticket` because `save` takes the `ticket` argument as value, 
+            // thus taking ownership of its value out of the caller function into the method.
+            // But we need `ticket`'s value after this method call, to verify it matches what we retrieve.
+            // Hence the need to clone it, creating a copy of the value and passing that copy to the `save` method.
+            //
+            // (You might have to go back to the `recap` koan to derive a couple more traits for Ticket and Status...)
+            store.save(ticket.clone(), ticket_id);
 
-            assert_eq!(store.data.get(&ticket_id).expect("Could not find the ticket").title, "A ticket title");
-
+            assert_eq!(store.get(&ticket_id), &ticket);
         }
 
-        /// Like HashMaps in other languages.
-        /// If you try and force retrieve a value for a key that does not exist
-        /// the HashMap will panic!
-        /// So make sure the value is there.
+        /// We want our `get` method to panic when looking for an id to which there is no ticket associated (for now).
         ///
-        /// Don't worry, Rust has a way to handle cases
-        /// where the HashMap tells us that a value does not exist.
-        /// We will take a look at that scenario a little later.
+        /// Rust has a way to handle this failure mode more gracefully, we will take a look at it later.
         #[test]
         #[should_panic]
         fn a_missing_ticket()
         {
-            let ticket = Ticket {
-                title: "A ticket title".into(),
-                description: "An enlightened description".into()
-            };
+            let ticket_store = TicketStore::new();
 
-            let mut store = create_ticket_store();
-
-            store.data.insert(1, ticket);
-
-            assert_eq!(store.data.get(&3).expect("Could not find the ticket").title, "A ticket title");
+            ticket_store.get(&1);
         }
-
     }
 }
