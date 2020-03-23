3ad14bc04d1fa54975368c94d467f982fd39c243
Luca Palmieri <lpalmieri@truelayer.com>
2020-03-23 21:55:07 +0000

    Rework comment.

--- jira-wip/src/koans/03_cli/02_the_end.rs
+++ jira-wip/src/koans/03_cli/02_the_end.rs
@@ -5,7 +5,8 @@ mod the_end {
     /// having fun toying around with rebuilding JIRA.
     ///
     /// If you have any feedback on the workshop, please reach out to rust@lpalmieri.com
-    /// If you found any typo, mistake or you think a section could be worded better, please open a PR!
+    /// If you found any typo, mistake or you think a section could be worded better,
+    /// please open a PR!
     ///
     /// ~ See you next time! ~
     ///
